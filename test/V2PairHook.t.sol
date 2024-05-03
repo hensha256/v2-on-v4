fd06eb5f6501bf97d11f2b89cfd23c4e99c71bfc
Alice Henshaw <henshawalice@gmail.com>
2024-05-03 10:57:46 -0400

    liquidity add tests

--- test/V2PairHook.t.sol
+++ test/V2PairHook.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.24;
 
 // Local
 import {V2PairHook} from "../src/V2PairHook.sol";
+import {AddLiquidityRouter} from "./mocks/AddLiquidityRouter.sol";
 // Solmate
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 // Forge
@@ -21,7 +22,7 @@ contract V2PairHookTest is Test {
     Currency currency0;
     Currency currency1;
     PoolManager manager;
-    address router;
+    AddLiquidityRouter liquidityRouter;
 
     function parameters() external view returns (Currency, Currency, IPoolManager) {
         return (currency0, currency1, manager);
@@ -30,12 +31,12 @@ contract V2PairHookTest is Test {
     function deployMintAndApprove2Currencies() internal {
         MockERC20 tokenA = new MockERC20("TEST", "TEST", 18);
         tokenA.mint(address(this), 2 ** 255);
-        tokenA.approve(router, type(uint256).max);
+        tokenA.approve(address(liquidityRouter), type(uint256).max);
         Currency currencyA = Currency.wrap(address(tokenA));
 
         MockERC20 tokenB = new MockERC20("TEST2", "TEST2", 18);
         tokenB.mint(address(this), 2 ** 255);
-        tokenB.approve(router, type(uint256).max);
+        tokenB.approve(address(liquidityRouter), type(uint256).max);
         Currency currencyB = Currency.wrap(address(tokenB));
 
         (currency0, currency1) = address(tokenA) < address(tokenB) ? (currencyA, currencyB) : (currencyB, currencyA);
@@ -43,6 +44,8 @@ contract V2PairHookTest is Test {
 
     function setUp() public {
         manager = new PoolManager(500000);
+        liquidityRouter = new AddLiquidityRouter(manager);
+
         deployMintAndApprove2Currencies();
 
         // The constructor will call back and fetch the manager and currencies
@@ -60,5 +63,17 @@ contract V2PairHookTest is Test {
         PoolKey memory key = PoolKey({currency0: currency0, currency1: currency1, fee: 0, tickSpacing: 1, hooks: hook});
 
         manager.initialize(key, SQRT_RATIO_1_1, "");
+    }
+
+    function test_mint_liquidity() public {
+        PoolKey memory key = PoolKey({currency0: currency0, currency1: currency1, fee: 0, tickSpacing: 1, hooks: hook});
+        manager.initialize(key, SQRT_RATIO_1_1, "");
+
+        uint256 balanceBefore = hook.balanceOf(address(this));
+        uint256 liquidity = liquidityRouter.addLiquidity(hook, 1e18, 1e18);
+        uint256 balanceAfter = hook.balanceOf(address(this));
+
+        assertEq(balanceBefore + liquidity, balanceAfter);
+        assertGt(liquidity, 0);
     }
 }
